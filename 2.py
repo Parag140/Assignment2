4f7d9792830cfe57fc10b280d016b88af072b95f
PARAG 22105105 <118710280+Parag140@users.noreply.github.com>
2022-12-22 20:21:49 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 22 12:55:59 2022
+
+@author: paraggupta
+"""
+
+name="parag"
+SID=22105105
+department="electronics and communication engineering"
+CGPA=10
+print("Hey,",name,"here!""\n""My SID is",SID,"\n""I am from",department,"department and my cgpa is",CGPA)
---
+++ 3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 22 03:07:23 2022
+
+@author: paraggupta
+"""
+a=56
+b=10
+
+print(a&b)
+print(a|b)
+print(a^b)
+print(a<<2)
+print(b<<2)
+print(a>>4)
+print(b>>4)
